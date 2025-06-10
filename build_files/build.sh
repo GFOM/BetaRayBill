964874a47e70237ae617356e5086720632ef6200
GFOM <96686409+GFOM@users.noreply.github.com>
2025-06-10 17:43:49 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install  @cosmic-desktop-environment -y 
+dnf5 install @cosmic-desktop-environment tio btop fastfetch hyfetch adb -y 
 
 # Use a COPR Example:
 #
@@ -18,12 +18,10 @@ dnf5 install  @cosmic-desktop-environment -y
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
-curl --tlsv1.3 -fsS https://repository.mullvad.net/rpm/stable/mullvad.repo | sudo tee /etc/yum.repos.d/mullvad.repo
-mkdir /var/opt/Mullvad VPN/resources/mullvad-setup
-rpm-ostree install mullvad-vpn mullvad-browser -y
+dnf5 config-manager addrepo --from-repofile=https://repository.mullvad.net/rpm/stable/mullvad.repo -y
+dnf5 install mullvad-browser -y
 
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
-#systemctl enable mullvad-daemon.service
-#systemctl enable mullvad-early-boot-blocking.service
+
